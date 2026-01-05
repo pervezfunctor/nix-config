83a1c18dcbee85251cfa7613b6fea59efda04401
Pervez Iqbal <pervezfunctor@gmail.com>
2026-01-06 03:26:33 +0530

    minor fixes

--- home/caelestia.nix
+++ home/caelestia.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, ... }:
 {
   modules = [
     inputs.caelestia.homeManagerModules.default
--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, ... }:
 {
   modules = [
     inputs.catppuccin.nixosModules.catppuccin
