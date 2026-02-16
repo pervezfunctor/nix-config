2beade9f3949aabe59554deb04316fe4f0ad3e5b
Pervez Iqbal <pervezfunctor@gmail.com>
2026-02-16 20:35:43 +0530

    minor changes

--- gnome.nix
+++ gnome.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   nixosModule = {
-    services.xserver.enable = true;
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
     security.pam.services.gdm.enableGnomeKeyring = true;
--- hosts/bd795/configuration.nix
+++ hosts/bd795/configuration.nix
@@ -28,7 +28,6 @@
 
   services.openssh.enable = true;
   security.rtkit.enable = true;
-  services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.printing.enable = true;
